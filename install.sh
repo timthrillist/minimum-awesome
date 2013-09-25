834dc3b9b289cb6f4b69c01d21504c24430a31e6
timthrillist <tmahoney@thrillist.com>
2013-09-25 19:44:57 +0000

    Linking files correctly for vundles to work

--- install.sh
+++ install.sh
@@ -6,6 +6,13 @@ mv ~/.vimrc ~/.vimrc.bak
 mv ~/.vimrc.bundles ~/.vimrc.bundles.bak
 mv ~/.vimrc.bundles.local ~/.vimrc.bundles.local.bak
 
+BASEDIR=$(dirname $0)
+
+ln $BASEDIR/tmux.conf ~/.tmux.conf
+ln $BASEDIR/vimrc ~/.vimrc
+ln $BASEDIR/vimrc.bundles ~/.vimrc.bundles
+ln $BASEDIR/vimrc.bundles.local ~/.vimrc.bundles.local
+
 # Get vundle for Vim ( Vim Bundles )
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
